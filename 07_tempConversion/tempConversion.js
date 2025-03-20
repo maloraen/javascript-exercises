5bcd9245009471701ead7eb1164a7211ecea2457
maloraen <158532343+maloraen@users.noreply.github.com>
2025-03-20 00:16:35 -0500

    completed
    basically just returning the result of the formula for each conversion,
    using the corresponding temperature abbreviation as the function parameter,
    and using Math.round to round to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) { // (F − 32) × 5/9
+  return Math.round(((f - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) { // (C * 9/5) + 32
+  return Math.round(((c * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line

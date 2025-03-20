d71056b758050be5769e8aac3cb075fc1642b0b9
maloraen <158532343+maloraen@users.noreply.github.com>
2025-03-19 23:55:01 -0500

    completed
    used if statements to check if the provided year is divisible by 4, or
    400 for century years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+ if ((year % 4 === 0) && (year % 100 !== 0)) {// if year is divisible by 4 but not a century year
+    return true;
+} else if ((year % 100 === 0) && (year % 400 === 0)) { // century year divisible by 400
+    return true;
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line

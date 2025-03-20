d42d1416b8a0fb1b8dfc446fae7deadbeed48871
maloraen <158532343+maloraen@users.noreply.github.com>
2025-03-19 22:47:07 -0500

    completed
    used .filter() method to iterate through provided array, with an arrow
    function used as the callback that iterates through the args and returns
    a new array with the args removed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(remove => !args.includes(remove));
 };
 
 // Do not edit below this line

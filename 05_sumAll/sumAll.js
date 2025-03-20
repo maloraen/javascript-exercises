52ed1c6233d84ef6fb10109b46c23b0913bcdd92
maloraen <158532343+maloraen@users.noreply.github.com>
2025-03-19 23:41:26 -0500

    completed
    used for loops inside of if/else statements to iterate from lesser value
    through to the greater value and add them to the overall sum to be returned
    
    include checks to make sure the provided arguments are not negative, and
    are valid integers and numbers. also checks to see which value is greater
    in order to make sure it still works if the larger number is provided first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
 
+    if ((x < 0) || (y < 0) || !Number.isInteger(x) || !Number.isInteger(y)) { // if x and/or y is negative or not a number or integer
+        return "ERROR";
+    } else if (x < y) { // if the second parameter is the larger one
+        for (i = x; i <= y; i++) {
+            sum += i;
+        }
+    } else if (y < x) { // if the first parameter is the larger one
+        for (i = y; i <= x; i++) {
+            sum += i;
+        }
+    } 
+
+    return sum;
 };
 
 // Do not edit below this line

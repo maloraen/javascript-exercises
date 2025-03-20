edb9c8f9cdd13030f4dde50b6803f500b243d0c3
maloraen <158532343+maloraen@users.noreply.github.com>
2025-03-20 00:24:20 -0500

    refined answer
    after reviewing the provided solution, and googling "how to reverse a string",
    and facepalming because i knew that, i changed the code to the split, reverse,
    join method, which, again, i knew but didn't use for some reason :')

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function(string) {
-    var newString  = "";
-
-    for (i = string.length - 1; i >= 0; i--) {
-        newString += string[i]; // add last character of string to newString, and continue backwards
-    }
-    return newString;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line

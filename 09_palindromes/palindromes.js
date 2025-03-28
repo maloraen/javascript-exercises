0550b4997d7ccb331fc4e6f8442e161eb1001264
maloraen <158532343+maloraen@users.noreply.github.com>
2025-03-27 19:53:28 -0500

    completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let noPunct = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()'"\s]/g, ""); // remove punction and special characters from string
+    let reversedStr = noPunct.split('').reverse().join(''); // reverse string without special characters
+    return reversedStr.toLowerCase() === noPunct.toLowerCase();
 };
 
 // Do not edit below this line
